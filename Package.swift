c8e1ca112f20fc0b19aef1cf1bb8abc19f0b3cc0
Joao Gabriel <joaogabriel@MacBook-Pro-de-Joao.local>
2025-10-30 22:08:19 -0300

    ajuste de versao

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AppRouter",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v18)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
